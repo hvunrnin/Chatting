df158af5f023617dc252491df64759786b2dd951
hvunrnin <hmkwon0919@cau.ac.kr>
2025-04-02 18:09:21 +0900

    feat : 메세지 저장 모델 (document) 정의

---
+++ src/main/java/test/chatting/domain/chatmessage/document/ChatMessageDocument.java
@@ -0,0 +1,37 @@
+package test.chatting.domain.chatmessage.document;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+import java.util.List;
+
+@Document(collection = "chat_messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessageDocument {
+
+    @Id
+    private String id; // MongoDB 기본 _id
+
+    private String roomId;
+
+    private String chunkId; // 예: "2025-04-02" (yyyy-MM-dd)
+
+    private List<Message> messages;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Message {
+        private String sender;
+        private String message;
+        private Instant timestamp;
+    }
+}
---
+++ src/main/java/test/chatting/domain/chatmessage/repository/ChatMessageMongoRepository.java
@@ -0,0 +1,15 @@
+package test.chatting.domain.chatmessage.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import test.chatting.domain.chatmessage.document.ChatMessageDocument;
+
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatMessageMongoRepository extends MongoRepository<ChatMessageDocument, String> {
+
+    Optional<ChatMessageDocument> findByRoomIdAndChunkId(String roomId, String chunkId);
+
+    List<ChatMessageDocument> findAllByRoomIdOrderByChunkIdDesc(String roomId);
+}
