5f891ed900f7ba938a82b114d77a2f78aaf98ba0
hvunrnin <hmkwon0919@cau.ac.kr>
2025-04-03 23:14:52 +0900

    faet : 각 채팅방 나가기 추가

--- src/main/java/test/chatting/domain/room/controller/RoomController.java
+++ src/main/java/test/chatting/domain/room/controller/RoomController.java
@@ -1,8 +1,11 @@
 package test.chatting.domain.room.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import test.chatting.domain.room.entity.ChatRoom;
+import test.chatting.domain.room.entity.RoomUser;
+import test.chatting.domain.room.repository.RoomUserRepository;
 import test.chatting.domain.room.service.ChatRoomService;
 
 import java.util.List;
@@ -13,6 +16,7 @@ import java.util.List;
 public class RoomController {
 
     private final ChatRoomService chatRoomService;
+    private final RoomUserRepository roomUserRepository;
 
     // 해당 roomId가 존재하면 채팅방 참여, 존재하지 않으면 채팅방 생성
     @PostMapping("/join-or-create")
@@ -27,5 +31,11 @@ public class RoomController {
     @GetMapping("/list")
     public List<ChatRoom> getMyRooms(@RequestParam String userId) {
         return chatRoomService.getRoomsByUser(userId);
+    }
+
+    @DeleteMapping("/leave")
+    public ResponseEntity<Void> leaveRoom(@RequestParam String roomId, @RequestParam String userId) {
+        roomUserRepository.deleteById(new RoomUser.RoomUserId(roomId, userId));
+        return ResponseEntity.ok().build();
     }
 }
